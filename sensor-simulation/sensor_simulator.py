c72151833ea18abfc8a10cb95f3de3d509e1eb1a
kare0041 <kare0041@algonquinlive.com>
2025-04-13 17:22:20 -0400

    adding changes on sensor_simulator.py

--- sensor-simulation/sensor_simulator.py
+++ sensor-simulation/sensor_simulator.py
@@ -1,7 +1,7 @@
 import random, time, datetime, json
-# from azure.iot.device import IoTHubDeviceClient, Message
+from azure.iot.device import IoTHubDeviceClient, Message
 
-CONNECTION_STRINGS = ["Your IoT Device Connection String", "Your IoT Device Connection String","Your IoT Device Connection String"]
+CONNECTION_STRINGS = ["HostName=danieliotcst8916.azure-devices.net;DeviceId=sensor_dows_lake;SharedAccessKey=esSycMr9FxqEu5wCCaPLF4AzYlwLg7hyjugpoX9ik6Y=", "HostName=danieliotcst8916.azure-devices.net;DeviceId=sensor_fifth_avenue;SharedAccessKey=d0WiV18U/x1ow+RX5+4NKP27Yyg+rWuNNmROVTjt3Ik=","HostName=danieliotcst8916.azure-devices.net;DeviceId=sensor_nac;SharedAccessKey=NglXDacC7QckPwefDzvJ5PS6SD3SsIZhy5hQwcRYP1s="]
 
 LOCATIONS = ["Dow's Lake", "Fifth Avenue", "NAC"]
 
@@ -15,23 +15,25 @@ def generate_payload(location):
         "timestamp": datetime.datetime.utcnow().isoformat() + "Z"
     }
 def main():
-    # clients = {
-    #     "Dow's Lake": IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRINGS[0]),
-    #     "Fifth Avenue": IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRINGS[1]),
-    #     "NAC": IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRINGS[2])
-    # }
+    clients = {
+        "Dow's Lake": IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRINGS[0]),
+        "Fifth Avenue": IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRINGS[1]),
+        "NAC": IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRINGS[2])
+    }
     try:
         while True:
             for location in LOCATIONS:
                 data = generate_payload(location)
-                # message = Message(json.dumps(data))
+                message = Message(json.dumps(data))
                 print(f"Sending: {data}")
-                # clients[location].send_message(message)
+                clients[location].send_message(message)
             time.sleep(10)
     except KeyboardInterrupt:
         print("Simulation stopped.")
     finally:
-        # client.shutdown()
+        clients["Dow's Lake"].shutdown()
+        clients["Fifth Avenue"].shutdown()
+        clients["NAC"].shutdown()
         print("Client shutdown.")
 
 if __name__ == "__main__":
